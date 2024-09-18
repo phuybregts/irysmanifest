c4674a45c3bc2245addcb06f0040dd6335d0da3e
ph <phuybregts@gmail.com>
2024-09-18 16:17:28 +0200

    clone ready

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,50 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+// import { Uploader } from "@irys/upload";
+// import { Ethereum } from "@irys/upload-ethereum";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const exampleManifestID = "G2RYHJXUhqamBxc1A3cFjoNbpBih9UN6fS7udRYCBxST";
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  const accountActive = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+
+  const downloadOriginalManifest = async (exampleManifestID) => {
+    try {
+      const response = await fetch(
+        `https://gateway.irys.xyz/mutable/${exampleManifestID}`
+      );
+      if (!response.ok) throw new Error("Failed to fetch original manifest");
+      return response.json();
+    } catch (error) {
+      console.error("Error downloading original manifest", error);
+      throw error;
+    }
+  };
+
+  async function runCode() {
+    // Via online docs
+    const originalManifest = await downloadOriginalManifest(exampleManifestID);
+    console.log(originalManifest);
+    const userDataManifestPathID2 = originalManifest[accountActive];
+    console.log(userDataManifestPathID2);
+
+    // Via alternative path
+
+    const gatewayUrlUserDataManifestURL = `https://gateway.irys.xyz/mutable/${exampleManifestID}`;
+    const fetchResponseUserDataManifest = await fetch(
+      gatewayUrlUserDataManifestURL
+    );
+    const dataUserDataManifest = await fetchResponseUserDataManifest.text();
+    const parsedUserDataManifestData = JSON.parse(dataUserDataManifest);
+    console.log(parsedUserDataManifestData);
+    const userDataManifestPathID =
+      parsedUserDataManifestData.paths[accountActive];
+    console.log(userDataManifestPathID);
+  }
+
+  runCode();
+
+  return <div className={styles.page}>Hirys</div>;
 }
